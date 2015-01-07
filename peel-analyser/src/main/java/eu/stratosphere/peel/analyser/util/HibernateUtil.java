28c2c923925ff8e750ded7618efbeae8bca3c5e8
fabsi10 <f.schuerer@campus.tu-berlin.de>
2015-01-07 23:26:58 +0100

    Started rewriting the ParserFlink test. Implemented the ORMUtil to hide most of ORM to avoid failures.

--- peel-analyser/src/main/java/eu/stratosphere/peel/analyser/util/HibernateUtil.java
+++ peel-analyser/src/main/java/eu/stratosphere/peel/analyser/util/HibernateUtil.java
@@ -1,23 +1,50 @@
 package eu.stratosphere.peel.analyser.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.reflections.Reflections;
+import scala.tools.nsc.backend.icode.Primitives;
+
+import java.util.Set;
 
 /**
  * Created by Fabsi on 21.10.2014.
  */
 public class HibernateUtil {
-    public static ORM ormUtil = null;
+    private static final ORMUtil ormUtil = new ORMUtil();
+    private static final SessionFactory sessionFACTORY = buildSessionFactory();
 
-    public static ORM getORM(){
-        if(ormUtil == null)
-            initORM();
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable e){
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFACTORY(){
+        return sessionFACTORY;
+    }
+
+    public static org.hibernate.Session getSession(){
+        if(sessionFACTORY.getCurrentSession() == null){
+            return sessionFACTORY.openSession();
+        } else {
+            return sessionFACTORY.getCurrentSession();
+        }
+    }
+
+    public static ORMUtil getORM(){
         return ormUtil;
     }
 
-    public static void initORM() {
-        ormUtil = new ORMUtil();
-    }
     public static void deleteAll() {
-        ORMUtil.createSessionFactory();
+        String deleteString = "delete * from :modelname";
+        Reflections reflections = new Reflections("eu.stratosphere.peel.analyser.model");
+        Set<Class<? extends Object>> models = reflections.getSubTypesOf(Object.class);
+        for(Class<? extends Object> model: models){
+            ormUtil.executeQuery(model, deleteString, new QueryParameter("modelname", model.getName()));
+        }
     }
 
 }
